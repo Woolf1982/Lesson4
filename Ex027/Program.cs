b77f067abed13c9d59cb160ddcb5f7cbcdd65a34
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-11-27 12:53:52 +0300

    Ex027

---
+++ Ex027/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Enter some number N: ");
+int SomeNumber = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int SomeNumber)
+  {
+    int counter = Convert.ToString(SomeNumber).Length;
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++)
+    {
+      advance = SomeNumber - SomeNumber % 10;
+      result = result + (SomeNumber - advance);
+      SomeNumber = SomeNumber / 10;
+    }
+    return result;
+  }  
+int sumNumber = SumNumber(SomeNumber);
+Console.WriteLine("The sum of the digits in a number: " + sumNumber);  
