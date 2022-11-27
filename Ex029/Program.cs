224332b81a0dea9937113c8752ba2f430b425c73
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-11-27 13:10:04 +0300

    Ex029

---
+++ Ex029/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 29:
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] GetRange( int Length)
+{
+    int [] array = new int [Length];
+    Random rd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rd.Next(0, 50);
+    }
+    return array;
+}
+
+void PrintArray( int [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int [] array = GetRange(8);
+PrintArray(array);
